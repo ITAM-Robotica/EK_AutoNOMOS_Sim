91f532526309b87cf0f3110b87a9491219d079f5
Edgar Granados Osegueda <edgar.granados@itam.mx>
2017-11-14 13:05:14 -0600

    changed the tf method and implemented PI ctrl for steering

--- src/autonomos_simulation/autoNOMOS_gazebo_bridge.cpp
+++ src/autonomos_simulation/autoNOMOS_gazebo_bridge.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Float64.h>
 #include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
@@ -10,6 +11,9 @@
 #include "gazebo_msgs/LinkStates.h"
 #include "tf/transform_datatypes.h"
 #include "tf/LinearMath/Matrix3x3.h"
+#include <tf2_ros/transform_listener.h>
+#include <tf/transform_listener.h>
+#include "geometry_msgs/PoseStamped.h"
 
 #define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
@@ -19,33 +23,56 @@
 // 		dummy.angular.z
 geometry_msgs::Twist robot_position;
 geometry_msgs::Twist velocity_msg;
+
+geometry_msgs::PoseStamped pose_stp_w;
+geometry_msgs::PoseStamped pose_stp_ack;
+
 std::string car_name = "ackermann";
+std::string world = "world";
 //rate_hz assignment
-double rate_hz = 1;
+double rate_hz = 30;
 int des_steering = 0;
 int speed = 0;
 float curr_steering = 0;
 float err = 0;
 float sum_err = 0;
-float kp = .5;
-float ki = .01;
+float kp = .1;
+float ki = .0;
 
 int set_steering_force()
 {
 	
-	err = curr_steering - des_steering;
+	err = des_steering - curr_steering;
 	sum_err += err;
+	float pi = kp * err + ki * sum_err; 
+	
+	// if( pi > 1)
+	// {
+	// 	pi = 1;
+	// }
+	// if (pi < -1)
+	// {
+	// 	pi = -1;
+	// }
 
 	ROS_INFO_STREAM("des: " << des_steering << " curr: " << curr_steering);
-	ROS_INFO_STREAM("err: " << err << " sum_err: " << sum_err << " tot: " << kp * err + ki * sum_err );
+	ROS_INFO_STREAM("err: " << err << " sum_err: " << sum_err << " tot: " << pi );
 
-	return kp * err + ki * sum_err ;
+	return  pi;
 }
 
 
 // manual_control/steering  (std_msgs/Int16)
 void get_steering(const std_msgs::Int16& msg) {
 	 des_steering = msg.data;
+	 // if(des_steering > 70)
+	 // {
+	 // 	ROS_WARN("steering > 70. Using 70." );
+	 // 	des_steering = 70;
+	 // } else if (des_steering < 20) {
+	 // 	ROS_WARN("steering < 20. Using 20.");
+	 // 	des_steering = 20;
+	 // }
 }
 
 void get_speed(const std_msgs::Int16& msg) {
@@ -86,23 +113,41 @@ void get_curr_link_states(const gazebo_msgs::LinkStates& msg)
 	// ROS_INFO_STREAM(msg.pose[steering_link].orientation) ;
 	// ROS_INFO_STREAM(q);
 	tf::Matrix3x3 m(q);
+
+	pose_stp_w.header.frame_id = "ackermann";
+	pose_stp_w.pose.position = msg.pose[steering_link].position;
+	pose_stp_w.pose.orientation = msg.pose[steering_link].orientation;
 	
 	m.getRPY(roll, pitch, yaw); //USING yaw
-	ROS_DEBUG_STREAM("( " << roll << " , " << pitch << " , " << yaw << " )");
-
-	curr_steering = yaw;
+	ROS_INFO_STREAM("( " << roll << " , " << pitch << " , " << -yaw << " )");
+	ROS_INFO_STREAM("rads: " << -yaw << " deg: " << -yaw * 180/ M_PI);
+	curr_steering = -yaw * 180 / M_PI;
 
 }
 int main(int argc, char **argv){
 	ros::init(argc,argv,"robot_velocity_node");
-	ros::NodeHandle nh;
+	ros::NodeHandle nh("~");
 	ROS_INFO_STREAM("robot_velocity_node initialized");
 	ROS_INFO_STREAM(ros::this_node::getName());
 
-	if(argc > 1)
-	{
-		car_name = argv[2];
-	}
+	// if (nh.getParam("st_kp", kp))
+	// {
+	// 	ROS_INFO_STREAM("The steering kp values is: " << kp);
+	// } else {
+	// 	kp = 0.1;
+	// 	ROS_INFO_STREAM("No steering kp received, using: kp = " << kp);
+	// }
+
+	nh.param<float>("st_kp", kp, 0.1);
+	ROS_INFO_STREAM("The steering kp values is: " << kp);
+
+	nh.param<float>("st_ki", ki, 0.0001);
+	ROS_INFO_STREAM("The steering ki values is: " << ki);
+	// if(argc > 1)
+	// {
+	// 	car_name = argv[2];
+	// }
+	nh.param<std::string>("car_name", car_name, "ackermann");
 	ROS_INFO_STREAM("Using gazebo model: " << car_name);
 	// Suscribe to Gazebo service ApplyJointEffor
 	ros::ServiceClient client = nh.serviceClient<gazebo_msgs::ApplyJointEffort>("/gazebo/apply_joint_effort");
@@ -111,7 +156,10 @@ int main(int argc, char **argv){
 
 	ros::Subscriber sub_steering = nh.subscribe("/manual_control/steering", 1, &get_steering);
 	ros::Subscriber sub_speed = nh.subscribe("/manual_control/speed", 1, &get_speed);
-	ros::Subscriber sub_curr_link_states = nh.subscribe("/gazebo/link_states", 1, &get_curr_link_states);
+	// ros::Subscriber sub_curr_link_states = nh.subscribe("/gazebo/link_states", 1, &get_curr_link_states);
+
+	ros::Publisher pub = nh.advertise<std_msgs::Float64> ("/car_steering_angle", 1);
+	std_msgs::Float64 msg_fl;
 
     //define the rate
 	ros::Rate rate(rate_hz);
@@ -119,7 +167,13 @@ int main(int argc, char **argv){
 	ros::Duration duration ;
 
 	double effort[4];
-	
+
+	tf::TransformListener tfListener;
+	tfListener.waitForTransform(world, car_name, ros::Time::now(), ros::Duration(3.0) );
+
+	tf::StampedTransform transform_steering;
+	double roll, pitch, yaw;
+
 	while (ros::ok())
 	{
 	// double* velMots = getMotorValue(velocity_msg.linear.x,velocity_msg.linear.y,velocity_msg.angular.z);
@@ -128,14 +182,40 @@ int main(int argc, char **argv){
 	// effort [1] = velMots[1];
 	// effort [2] = velMots[2];
 	// effort [3] = velMots[3];
-  
+		// ROS_INFO_STREAM("Before transformPose: " << pose_stp_w << pose_stp_ack);
+		try{
+			// tfListener.transformPose("ackermann", pose_stp_w, pose_stp_ack);
+			tfListener.lookupTransform("/ackermann", "/steer_link", ros::Time(0), transform_steering);
+			// ROS_INFO_STREAM("After transformPose: " << pose_stp_w << pose_stp_ack);
+			// ROS_INFO_STREAM("After transformPose: " );
+
+			tf::Quaternion q = transform_steering.getRotation();
+			// ROS_INFO_STREAM(msg.pose[steering_link].orientation) ;
+			// ROS_INFO_STREAM(q);
+			tf::Matrix3x3 m(q);
+		
+			m.getRPY(roll, pitch, yaw); //USING yaw
+			ROS_INFO_STREAM("-------------------------------------------------------");
+			ROS_INFO_STREAM("( " << transform_steering.getOrigin().x() << " , " << transform_steering.getOrigin().y() << " , " << transform_steering.getOrigin().z() << " )");
+			ROS_INFO_STREAM("( " << roll << " , " << pitch << " , " << yaw << " )");
+			ROS_INFO_STREAM("rads: " << -yaw << " deg: " << yaw * 180/ M_PI );
+			curr_steering = yaw * 180/ M_PI;
+			ROS_INFO_STREAM("-------------------------------------------------------");
+
+			// ROS_INFO_STREAM(transform_steering.getRotation() );
+		} catch (tf::TransformException ex){
+			ROS_ERROR("%s",ex.what());
+			// ROS_INFO_STREAM("ZERO Quaternion!!");
+		}
+		
+  		
 		if(client.exists()){
 			
 			start_time.sec = 0;
 			start_time.nsec = 0;
 			duration.sec = 1/rate_hz;
 			duration.nsec = 0;
-
+			// set_steering_force(); 
 			// Wheel-Joint 1
 			eff_msg[0].request.joint_name = "steer_joint";
 			eff_msg[0].request.duration = duration;
@@ -145,13 +225,13 @@ int main(int argc, char **argv){
 			// Wheel-Joint 2
 			eff_msg[1].request.joint_name = "back_left_wheel_joint";
 			eff_msg[1].request.duration = duration;
-			eff_msg[1].request.effort = effort[1];
+			eff_msg[1].request.effort = 0;
 			eff_msg[1].request.start_time = start_time;
 
 			// Wheel-Joint 3
 			eff_msg[2].request.joint_name = "back_right_wheel_joint";
 			eff_msg[2].request.duration = duration;
-			eff_msg[2].request.effort = effort[2];
+			eff_msg[2].request.effort = 0;
 			eff_msg[2].request.start_time = start_time;
 
 
@@ -165,7 +245,9 @@ int main(int argc, char **argv){
 			// " 4: " << ((eff_msg[3].response.success == 1) ? "TRUE" : "FALSE")
 			);
 		}																																																																																																
-		
+		msg_fl.data = curr_steering;
+		pub.publish(msg_fl);
+
 		ros::spinOnce();
 		rate.sleep();
     }
--- src/autonomos_simulation/tf2_broadcaster_node.cpp
+++ src/autonomos_simulation/tf2_broadcaster_node.cpp
@@ -8,6 +8,8 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
 #include "tf/tf.h"
 
 std::string robot_name;
@@ -15,34 +17,83 @@ std::string robot_name;
 //rate_hz assignment
 double rate_hz = 5;
 std::string world;
+gazebo_msgs::LinkStates links;
+gazebo_msgs::LinkStates models;
+ros::Time links_time ;
+
+// static tf2_ros::TransformBroadcaster br;
+void set_leaf(std::string parent_name, std::string child_name, geometry_msgs::Pose link_pose, geometry_msgs::Twist link_twist)
+{
+	geometry_msgs::TransformStamped transformStamped;
+	std::string parent, child, grandchildren;
+
+
+	// ROS_INFO_STREAM("Parent: " << parent_name << " , child: " << child_name);
+
+	// Recursive method to create tf tree:
+	// Base case: there is no child (child is empty).
+	if (!child_name.empty())
+	{
+		std::size_t char_pos = child_name.find("::"); 							// find the position of the first '::' (multiple chained links  ==> multiple '::')
+		parent = "world";// parent_name;													// the parent is alway parent_name
+		if(char_pos ==  std::string::npos)										// check if '::' wasnt found (position is npos)
+		{
+			child = child_name;													// this is the last child, asign it without checking for grandchildren
+			grandchildren = "";													// there are no more grand children
+		} else {
+			child = child_name.substr(0, char_pos);								// if '::' was found, split the child from the grandchildren
+			grandchildren = child_name.substr(char_pos+2, std::string::npos);	// get the grandchildren
+		}
+
+		if(parent_name.empty())													// Only in the first iteration the parent name can be empty
+		{
+			set_leaf(child, grandchildren, link_pose, link_twist);				// Recursion
+		} else {
+			static tf2_ros::TransformBroadcaster br;							// class to broadcast TF
+
+			transformStamped.header.stamp = links_time;							// assign the time of the pose
+			transformStamped.header.frame_id = parent;							// assign the parent
+			transformStamped.child_frame_id = child;							// assign the child
+
+			transformStamped.transform.translation.x = link_pose.position.x;	// assign the position x
+			transformStamped.transform.translation.y = link_pose.position.y;	// assign the position y
+			transformStamped.transform.translation.z = link_pose.position.z;	// assign the position z
+
+			transformStamped.transform.rotation.x = link_pose.orientation.x;	// assign the quaternion x
+			transformStamped.transform.rotation.y = link_pose.orientation.y;	// assign the quaternion y
+			transformStamped.transform.rotation.z = link_pose.orientation.z;	// assign the quaternion z
+			transformStamped.transform.rotation.w = link_pose.orientation.w;	// assign the quaternion w
+	
+			br.sendTransform(transformStamped);									// broadcast the TF
+
+			set_leaf(child, grandchildren, link_pose, link_twist);				// call for the next recursion
+		}
+
+	}
+
+}
 
 
 void poseCallback(const gazebo_msgs::LinkStates& msg){
-	static tf2_ros::TransformBroadcaster br;
-	geometry_msgs::TransformStamped transformStamped;
-	
-	double yaw = tf::getYaw(msg.pose[1].orientation);
-	yaw = yaw - M_PI/2;
-	//aux_tf.transform.rotation = tf::createQuaternionFromRPY(0,0,-M_PI/2);
 
-	transformStamped.header.stamp = ros::Time::now();
-	transformStamped.header.frame_id = world;
-	transformStamped.child_frame_id = robot_name;
+	models = msg;
 
-	transformStamped.transform.translation.x = msg.pose[1].position.x;
-	transformStamped.transform.translation.y = msg.pose[1].position.y;
-	transformStamped.transform.translation.z = msg.pose[1].position.z;
-	
-	// transformStamped.transform.rotation = tf::createQuaternionFromRPY(0, 0, yaw); //msg.pose[1].orientation;
-	tf::Quaternion q = tf::createQuaternionFromRPY(0, 0, yaw);
-	transformStamped.transform.rotation.x = q.x();
-	transformStamped.transform.rotation.y = q.y();
-	transformStamped.transform.rotation.z = q.z();
-	transformStamped.transform.rotation.w = q.w();
-    // transformStamped.transform.rotation = msg.pose[1].orientation;
+}
 
-    br.sendTransform(transformStamped);
+void set_links(const gazebo_msgs::LinkStates& msg)
+{
+	links = msg;
+	links_time = ros::Time::now();
+	// ROS_INFO_STREAM("Links: " << msg);
+}
 
+void stablish_tf()
+{
+	// ROS_INFO_STREAM("CREATING TREE:");
+	for (int i = 0; i < links.name.size(); ++i)
+	{
+		set_leaf(world, links.name[i], links.pose[i], links.twist[i]);
+	}
 }
 
 int main(int argc, char **argv){
@@ -56,6 +107,7 @@ int main(int argc, char **argv){
 	world = argc>2 ? argv[2] : "world";
 	
 	ros::Subscriber sub_robot_pos = nh.subscribe("/gazebo/model_states", 1, &poseCallback); 
+	ros::Subscriber sub_link_states = nh.subscribe("/gazebo/link_states", 1, &set_links); 
 
 	ROS_INFO_STREAM("World frame name is \"" << world << "\". Robot frame name is \"" << robot_name << "\".");
 	ROS_INFO_STREAM("Ready...");
@@ -64,6 +116,9 @@ int main(int argc, char **argv){
 	ros::Rate loop_rate(rate_hz);
 	while(nh.ok())
 	{
+
+
+		stablish_tf();
 		ros::spinOnce();
 		loop_rate.sleep();
 
