c062aae501a5dfb1aa4eb06d125839189c0f90c7
marhr <marianarhr13@gmail.com>
2018-04-09 20:46:11 -0500

    fixed bug in install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo "Begining instalation..."
-PWD=$(pwd)
+SIM_PWD=$(pwd)
 
 mkdir -p src/Gazebo_plugin/build
 cd src/Gazebo_plugin/build
 cmake ..
 make 
-cd $(PWD)
+cd $SIM_PWD 
 
 plugin="src/Gazebo_plugin/build"
 models="src/autonomos_gazebo_simulation/models"
 resource="src/autonomos_gazebo_simulation/worlds"
 
-plugin_path=$pwd$plugin
-echo "export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$PWD$plugin" >> ~/.bashrc
-echo "export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$PWD$models" >> ~/.bashrc
-echo "export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:$PWD$resource" >> ~/.bashrc
+#plugin_path=$HOME_PWD$plugin
+echo "export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$SIM_PWD$plugin" >> ~/.bashrc
+echo "export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$SIM_PWD$models" >> ~/.bashrc
+echo "export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:$SIM_PWD$resource" >> ~/.bashrc
 
 source ~/.bashrc
 #export GAZEBO_PLUGIN_PATH=/home/robotica/EK_AutoNOMOS/src/Gazebo_plugin/build
