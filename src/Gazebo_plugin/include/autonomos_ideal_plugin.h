6ef41b633ff1044cc7a122592247caaad0ed08c8
Edgar Granados Osegueda <edgar.granados@itam.mx>
2019-04-12 16:16:17 -0500

    ideal plugin not workin, must change to kinematic mode

--- src/Gazebo_plugin/include/autonomos_ideal_plugin.h
+++ src/Gazebo_plugin/include/autonomos_ideal_plugin.h
@@ -1,96 +1,140 @@
 #ifndef _AUTONOMOS_IDEAL_PLUGIN_HH_
 #define _AUTONOMOS_IDEAL_PLUGIN_HH_
 
-#include <gazebo/gazebo.hh>
-#include <gazebo/physics/physics.hh>
+// std
 #include <thread>
+
+// ros
 #include <ros/ros.h>
 #include <ros/callback_queue.h>
 #include <ros/subscribe_options.h>
-#include <geometry_msgs/Pose2D.h>
-#include <std_msgs/Float32.h>
+
+// msgs
 #include <std_msgs/Int16.h>
+#include <std_msgs/Float32.h>
+#include <geometry_msgs/Pose2D.h>
+#include "gazebo_plugin/trajectory_segment.h"
+
+// gazebo
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
 #include <gazebo_plugins/PubQueue.h>
 
-namespace gazebo
+// ign
+#include <ignition/math.hh>
+
+#define WHEEL_PERIMETER M_PI * 0.03 * 2 // PI * DIAM = PI * RADIUS * 2
+
+using namespace gazebo;
+/// \brief A plugin to control a Velodyne sensor.
+class autonomos_ideal_plugin : public ModelPlugin
 {
-  /// \brief A plugin to control a Velodyne sensor.
-  class autonomos_plugin : public ModelPlugin
-  {
-    /// \brief Constructor
-    public: 
-      autonomos_plugin();
+  /// \brief Constructor
+  public: 
+    autonomos_ideal_plugin();
 
-      ~autonomos_plugin();
+    ~autonomos_ideal_plugin();
 
-
-
-    /// \brief The load function is called by Gazebo when the plugin is
-    /// inserted into simulation
-    /// \param[in] _model A pointer to the model that this plugin is
-    /// attached to.
-    /// \param[in] _sdf A pointer to the plugin's SDF element.
-      virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+    /**
+     * @brief Called when the plugin is inserted
+     * @details The load function is called by Gazebo when the plugin is inserted into simulation
+     * 
+     * @param _model A pointer to the model that this plugin is
+     * @param _sdf A pointer to the plugin's SDF element
+     */
+    virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
     /// \brief Set the velocity of the Velodyne
     /// \param[in] _vel New target velocity
-      void SetPosition(const double &_pos);
-    /// \brief Handle an incoming message from ROS
-    /// \param[in] _msg A float value that is used to set the velocity
-    /// of the Velodyne.
-      void OnRosMsg_steering(const std_msgs::Int16ConstPtr &_msg);
-
-      void OnRosMsg_vel(const std_msgs::Int16ConstPtr &_msg);
-      
-      void OnUpdate(const common::UpdateInfo & /*_info*/);
+    void SetPosition();
+    
+    void drive();
+     
+    void OnUpdate(const common::UpdateInfo & /*_info*/);
     /// \brief ROS helper function that processes messages
-    private: 
-      void QueueThread();
+  private: 
+    ///////////////
+    // FUNCTIONS //
+    ///////////////
 
-      void autonomos_connect();
+    void QueueThread();
 
-      void autonomos_disconnect();
+    void autonomos_connect();
 
-      float position;
+    void autonomos_disconnect();
 
-      float vel;
+    void move_robot();
 
-      ros::Publisher pub_;
-      PubQueue<geometry_msgs::Pose2D>::Ptr pub_queue_;
+    ///////////////
+    // VARIABLES //
+    ///////////////
+
+    common::Time seg_init_time;
+    common::Time seg_fin_time;
+
+    bool waiting_valid_traj;
+
+    float vel;
+
+    float steering_pos_traget;
+    float velocity_target;
+
+    std::string steering_name;
+    std::string left_wheel_name;
+    std::string right_wheel_name;
+
+    ros::Publisher pub_;
+    PubQueue<geometry_msgs::Pose2D>::Ptr pub_queue_;
       
-      PubMultiQueue pmq;
+    PubMultiQueue pmq;
       
-      event::ConnectionPtr updateConnection;
+    event::ConnectionPtr updateConnection;
+
     /// \brief Pointer to the model.
-      physics::ModelPtr model;
+    physics::ModelPtr model;
+
+    physics::WorldPtr world;
 
     /// \brief Pointer to the joint.
-      physics::JointPtr joint;
-      physics::JointPtr joint_left_wheel;
-      physics::JointPtr joint_right_wheel;
+    physics::JointPtr joint_steering;
+    physics::JointPtr joint_left_wheel;
+    physics::JointPtr joint_right_wheel;
+
+    physics::JointControllerPtr joint_controller;
+
 
     /// \brief A PID controller for the joint.
-      common::PID pid;
-      common::PID pid_vel_left;
-      common::PID pid_vel_right;
+    common::PID pid_steering;
+    common::PID pid_vel_left;
+    common::PID pid_vel_right;
+    common::PID pid_vel_model;
 
     /// \brief A node use for ROS transport
-      std::unique_ptr<ros::NodeHandle> rosNode;
+    std::unique_ptr<ros::NodeHandle> rosNode;
 
-      common::Time prevUpdateTime;
+    common::Time prevUpdateTime;
+    common::Time current_time;
+    common::Time step_time;
     /// \brief A ROS subscriber
-      ros::Subscriber rosSub;
-      ros::Subscriber rosSub_vel;
+    ros::Subscriber rosSub;
+    
+    ros::Subscriber rosSub_vel;
 
-      ros::NodeHandle _nh;
+    ros::ServiceClient ros_service_client;
+    ros::NodeHandle _nh;
         
     /// \brief A ROS callbackqueue that helps process messages
-      ros::CallbackQueue rosQueue;
+    ros::CallbackQueue rosQueue;
         
     /// \brief A thread the keeps running the rosQueue
-      std::thread rosQueueThread;
+    std::thread rosQueueThread;
 
-  };
-        
-}
+    // thread for moving the robot
+    std::thread ideal_robot_move;
+
+    gazebo_plugin::trajectory_segment trj_seg_msg;
+
+    ignition::math::Vector3d linear_vel;
+};
+    
 #endif
--- src/Gazebo_plugin/src/autonomos_ideal_plugin.cc
+++ src/Gazebo_plugin/src/autonomos_ideal_plugin.cc
@@ -1,232 +1,287 @@
 #include "autonomos_ideal_plugin.h"
-//#include <gazebo/gazebo.hh>
-//#include <gazebo/physics/physics.hh>
-//#include <thread>
-//#include <ros/ros.h>
-//#include <ros/callback_queue.h>
-//#include <ros/subscribe_options.h>
-//#include <std_msgs/Float32.h>
-//#include <std_msgs/Int16.h>
 
-#define ADJ_FACT 1 / 12 // original 1/50
+using std::cout;
+using std::endl;
+using std::string;
+using namespace gazebo;
 
-namespace gazebo
+// Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
+GZ_REGISTER_MODEL_PLUGIN(autonomos_ideal_plugin)
+
+autonomos_ideal_plugin::autonomos_ideal_plugin()
 {
-  // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
-  GZ_REGISTER_MODEL_PLUGIN(autonomos_ideal_plugin)
-
-  autonomos_plugin::autonomos_plugin()
-  {
-
-  }
-
-  autonomos_plugin::~autonomos_plugin()
-  {
-
-  }
-
-  /// \brief The load function is called by Gazebo when the plugin is
-  /// inserted into simulation
-  /// \param[in] _model A pointer to the model that this plugin is
-  /// attached to.
-  /// \param[in] _sdf A pointer to the plugin's SDF element.
-  void autonomos_plugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
-  {
-    // Safety check
-    if (_model->GetJointCount() == 0)
-    {
-      std::cerr << "Invalid joint count, autonomos_plugin not loaded\n";
-      return;
-    }
-
-     this->pmq.startServiceThread();
-
-    this->model = _model;
-
-    // Get the first joint. We are making an assumption about the model
-    // having one joint that is the rotational joint.
-    // this->joint = _model->GetJoint("steer_joint");
-
-    // this->joint_left_wheel = _model->GetJoint("back_left_wheel_joint");
-    // this->joint_right_wheel = _model->GetJoint("back_right_wheel_joint");
-
-    // std::cout << val << std::endl;
-
-    // std::cout << this->joint->GetName() << std::endl;
-
-
-    // Setup a P-controller, with a gain of 0.1.
-    // this->pid = common::PID(0.1, 0.001, 0.001);
-    // this->pid_vel_left = common::PID(0.1, 0.001, 0.001);
-    // this->pid_vel_right = common::PID(0.1, 0.001, 0.001);
-
-    // Apply the P-controller to the joint.
-    // this->model->GetJointController()->SetPositionPID(
-    //   this->joint->GetScopedName(), this->pid);
-
-    // this->model->GetJointController()->SetVelocityPID(
-    //   this->joint_left_wheel->GetScopedName(), this->pid_vel_left);
-
-    // this->model->GetJointController()->SetVelocityPID(
-    //   this->joint_right_wheel->GetScopedName(), this->pid_vel_right);
-
-    this->position = 0;
-    this->vel = 0;
-
-    if (!ros::isInitialized())
-    {
-      int argc = 0;
-      char **argv = NULL;
-      ros::init(argc, argv, "gazebo_client",
-        ros::init_options::NoSigintHandler);
-    }
-
-        // Create our ROS node. This acts in a similar manner to
-        // the Gazebo node
-    this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
-
-        // Create a named topic, and subscribe to it.
-    ros::SubscribeOptions so =
-    ros::SubscribeOptions::create<std_msgs::Int16>(
-      "/" + this->model->GetName() + "/ideal_control/steering",
-      1,
-      boost::bind(&autonomos_plugin::OnRosMsg_steering, this, _1),
-      ros::VoidPtr(), &this->rosQueue);
-    this->rosSub = this->rosNode->subscribe(so);
-
-    ros::SubscribeOptions so_vel =
-    ros::SubscribeOptions::create<std_msgs::Int16>(
-      "/" + this->model->GetName() + "/ideal_control/speed",
-      1,
-      boost::bind(&autonomos_plugin::OnRosMsg_vel, this, _1),
-      ros::VoidPtr(), &this->rosQueue);
-    this->rosSub_vel = this->rosNode->subscribe(so_vel);
-
-
-
-    // Spin up the queue helper thread.
-    this->rosQueueThread =
-    std::thread(std::bind(&autonomos_plugin::QueueThread, this));
-
-    // Listen to the update event. This event is broadcast every
-    // simulation iteration.
-    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-      boost::bind(&autonomos_plugin::OnUpdate, this, _1));
-
-     ros::AdvertiseOptions ao =
-      ros::AdvertiseOptions::create<geometry_msgs::Pose2D>(
-      "/" + this->model->GetName() + "/real_pose_from_gazebo", 1,
-      boost::bind(&autonomos_plugin::autonomos_connect, this),
-      boost::bind(&autonomos_plugin::autonomos_disconnect, this),
-      ros::VoidPtr(), NULL);
-
-      this->pub_ = this->rosNode->advertise(ao);
-
-      
-      this->pub_queue_ = this->pmq.addPub<geometry_msgs::Pose2D>();
-  }
-
-  /// \brief Set the velocity of the Velodyne
-  /// \param[in] _vel New target velocity
-  void autonomos_plugin::autonomos_connect()
-  {
-    printf("At: %s\n",__PRETTY_FUNCTION__);
-  }
-
-  void autonomos_plugin::autonomos_disconnect()
-  {
-    printf("At: %s\n",__PRETTY_FUNCTION__);    
-  }
-
-  void autonomos_plugin::SetPosition(const double &_pos)
-  {
-
-  }
-
-  /// \brief Handle an incoming message from ROS
-  /// \param[in] _msg A float value that is used to set the velocity
-  /// of the Velodyne.
-  void autonomos_plugin::OnRosMsg_steering(const std_msgs::Int16ConstPtr &_msg)
-  {
-
-      // carro real config:
-      // https://github.com/AutoModelCar/model_car/blob/version-3.1-kinetic/catkin_ws/src/fub_steering_calibration/scripts/SteerAngleActuator.xml
-      // command 0  : 0.3967852340780831    //  22.7341193   // izquierda
-      // command 30 : 0.28655792116953055   //  16.418559
-      // command 60 : 0.13636611612784297   //  7.8132029
-      // command 90 : -0.01                 //  -0.572957    // centro
-      // command 120: -0.139860631021484    //  -8.013423
-      // command 150: -0.2675472287070919   //  -15.329327
-      // command 180: -0.41853095368099036  //  -23.980057   // derecha
-
-      // sin corrección
-      //    this->position = _msg->data - 90;
-      // aproximación lineal a angulos reales del autonomos
-      this->position = 0.252556 * (_msg->data - 90) + 0.572957; // eliminado signo negativo a ver si con eso
-
-      std::cout << "On OnRosMsg_steering (new): " << this->position << ", msg_rcv: " << _msg->data << std::endl;
-
-  }
-
-  void autonomos_plugin::OnRosMsg_vel(const std_msgs::Int16ConstPtr &_msg)
-  {
-    // this->vel = _msg->data;
-    this->vel = _msg->data * -15 / 31 * ADJ_FACT;
-
-    std::cout << "On OnRosMsg_vel (new): " << this->vel << ", msg_rcv: " << _msg->data << std::endl;
-  }
-
-  void autonomos_plugin::OnUpdate(const common::UpdateInfo &)
-  {    
-    // compute the steptime for the PID
-    common::Time currTime = this->model->GetWorld()->GetSimTime();
-    common::Time stepTime = currTime - this->prevUpdateTime;
-    this->prevUpdateTime = currTime;
-
-    // set the current position of the joint, and the target position, 
-    // and the maximum effort limit
-    float pos_target = this->position;
-    float pos_curr = this->joint->GetAngle(0).Degree();
-
-    float vel_target = this->vel;
-    float vel_curr_left = this->joint_left_wheel->GetVelocity(0);
-    float vel_curr_right = this->joint_right_wheel->GetVelocity(0);
-
-    // calculate the error between the current position and the target one
-    double pos_err = pos_curr - pos_target;
-    double vel_err_left = vel_curr_left - vel_target;
-    double vel_err_right = vel_curr_right - vel_target;
-
-    // compute the effort via the PID, which you will apply on the joint
-    double effort_cmd = this->pid.Update(pos_err, stepTime);
-    double effort_cmd_vl = this->pid_vel_left.Update(vel_err_left, stepTime);
-    double effort_cmd_vr = this->pid_vel_right.Update(vel_err_right, stepTime);
-
-    // apply the force on the joint
-    this->joint->SetForce(0, effort_cmd);
-    this->joint_left_wheel->SetForce(0, effort_cmd_vl);
-    this->joint_right_wheel->SetForce(0, effort_cmd_vr);
-
-    double x,y,z;
-    gazebo::math::Pose pose;     
-    pose = this->model->GetWorldPose();
-    math::Vector3 v(0, 0, 0);
-
-    geometry_msgs::Pose2D pose_msg;
-    pose_msg.x = pose.pos.x;
-    pose_msg.y = pose.pos.y;
-    pose_msg.theta = pose.rot.GetYaw();
-
-    this->pub_queue_->push(pose_msg, this->pub_);
-  }
-      
-  /// \brief ROS helper function that processes messages
-  void autonomos_plugin::QueueThread()
-  {
-    static const double timeout = 0.01;
-    while (this->rosNode->ok())
-    {
-      this->rosQueue.callAvailable(ros::WallDuration(timeout));
-    }
-  }      
+  waiting_valid_traj = true;
 }
+
+autonomos_ideal_plugin::~autonomos_ideal_plugin()
+{
+}
+
+/// \brief The load function is called by Gazebo when the plugin is
+/// inserted into simulation
+/// \param[in] _model A pointer to the model that this plugin is
+/// attached to.
+/// \param[in] _sdf A pointer to the plugin's SDF element.
+void autonomos_ideal_plugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
+  cout << "autonomos_ideal_plugin: Loading plugin..." << endl;
+
+  // Safety check
+  if (_model->GetJointCount() == 0)
+  {
+    std::cerr << "Invalid joint count, autonomos_plugin not loaded\n";
+    return;
+  }
+
+  this->pmq.startServiceThread();
+
+  this -> model = _model;
+  this -> world = _model -> GetWorld();
+
+  this -> joint_steering    = _model -> GetJoint("steer_joint");
+  this -> joint_left_wheel  = _model -> GetJoint("back_left_wheel_joint");
+  this -> joint_right_wheel = _model -> GetJoint("back_right_wheel_joint");
+
+  this -> steering_name = this -> joint_steering -> GetScopedName();
+  this -> left_wheel_name = this -> joint_left_wheel -> GetScopedName();
+  this -> right_wheel_name = this -> joint_right_wheel -> GetScopedName();
+  // this->position = 0;
+  // this->vel = 0;
+
+  if (!ros::isInitialized())
+  {
+    int argc = 0;
+    char **argv = NULL;
+    ros::init(argc, argv, "gazebo_client",
+      ros::init_options::NoSigintHandler);
+  }
+  cout << "autonomos_ideal_plugin: ROS initialized..." << endl;
+
+  // Create our ROS node. This acts in a similar manner to the Gazebo node
+  ros::NodeHandle nh("autonomos_ideal_plugin"); 
+  this->rosNode.reset(&nh);
+  
+  trj_seg_msg.request.seq = 0;
+
+  // ros::ServiceClientOptions sco = ros::ServiceClientOptions::init(srv_name, false, header);
+  this -> ros_service_client = nh.serviceClient<gazebo_plugin::trajectory_segment>("/navigation/get_next_control");
+
+  // Spin up the queue helper thread.
+  this->rosQueueThread = std::thread(std::bind(&autonomos_ideal_plugin::QueueThread, this));
+
+  // this -> ideal_move_thread = std::thread(std::bind(&autonomos_ideal_plugin::move_robot, this));
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+    boost::bind(&autonomos_ideal_plugin::OnUpdate, this, _1));
+
+  // this -> joint_steering -> SetPosition(2, 0);
+  this -> pid_steering. Init(0.1, 0.001, 0.001, 10000, -10000);
+  this -> pid_vel_left. Init(1, 0.001, 0.001, 10000, -10000);
+  this -> pid_vel_right.Init(1, 0.001, 0.001, 10000, -10000);
+  this -> pid_vel_model.Init(500, 100, 1, 100000000, -100000000);
+
+  this -> joint_controller = this -> model -> GetJointController();
+
+  this -> joint_controller -> SetPositionPID(
+    this -> steering_name, this -> pid_steering);
+
+  this -> joint_controller -> SetVelocityPID(
+    this -> left_wheel_name, this -> pid_vel_left);
+
+  this -> joint_controller -> SetVelocityPID(
+    this -> right_wheel_name, this -> pid_vel_right);
+
+  this -> steering_pos_traget = 0;
+  this -> velocity_target = 0;
+
+  // this -> model -> SetJointPosition("steer_joint", 0);
+
+
+  cout << "autonomos_ideal_plugin: plugin loaded" << endl;
+
+  
+}
+
+void autonomos_ideal_plugin::move_robot()
+{
+
+}
+
+/// \brief Set the velocity of the Velodyne
+/// \param[in] _vel New target velocity
+void autonomos_ideal_plugin::autonomos_connect()
+{
+  printf("At: %s\n",__PRETTY_FUNCTION__);
+}
+
+void autonomos_ideal_plugin::autonomos_disconnect()
+{
+  printf("At: %s\n",__PRETTY_FUNCTION__);    
+}
+
+void autonomos_ideal_plugin::SetPosition()
+{
+  float pos_curr = 0;
+  double pos_err = 0;
+  double effort_cmd = 0;
+
+  #if GAZEBO_VERSION_MAJOR >= 8
+    ignition::math::Angle angle_aux(this -> joint_steering -> Position(0)); // in radians
+    pos_curr = angle_aux.Degree();
+  #else
+    pos_curr = this -> joint -> GetAngle(0).Degree();
+  #endif
+
+  pos_err = pos_curr - this -> steering_pos_traget;
+
+  effort_cmd = this -> pid_steering.Update(pos_err, this -> step_time);
+
+  this -> joint_steering -> SetForce(0, effort_cmd);
+
+}
+
+void autonomos_ideal_plugin::drive()
+{
+  float vel_curr = 0;
+  float error_x = 0;
+  double vel_err = 0;
+  double pid_res = 0;
+  float vel_curr_left = 0;
+  float vel_curr_right = 0;
+  double vel_err_left  = 0;
+  double vel_err_right = 0;
+  double effort_cmd_vl = 0;
+  double effort_cmd_vr = 0;
+
+  #if GAZEBO_VERSION_MAJOR >= 8
+    ignition::math::Vector3d linear_vel = this -> model -> RelativeLinearVel();
+    vel_curr = linear_vel.X();
+  #else
+    gazebo::math::Vector3d linear_vel = this -> model -> RelativeLinearVel();
+    vel_curr = linear_vel.x;
+  #endif
+
+  error_x = this -> velocity_target - vel_curr;
+
+  // cout << "\tVelocity: " << trj_seg_msg.response.speed << endl;
+
+  pid_res = -1 * this -> pid_vel_model.Update(error_x, this -> step_time);
+  
+  this -> joint_controller -> SetVelocityTarget(this -> left_wheel_name, pid_res);
+  this -> joint_controller -> SetVelocityTarget(this -> right_wheel_name, pid_res);
+  
+  vel_curr_left = this -> joint_left_wheel -> GetVelocity(0);
+  // vel_curr_right = this -> joint_right_wheel -> GetVelocity(0);
+
+  vel_err_left  = vel_curr_left - this -> velocity_target;
+  // vel_err_right = vel_curr_right - pid_res;
+
+  // effort_cmd_vl = this -> pid_vel_left.Update(vel_err_left, this -> step_time);
+  cout << "\tErr_vel : " << error_x << "\tpid_res: " << pid_res << endl;
+  cout << "\tErr_left: " << vel_err_left << "\tVel_real: " << vel_curr_left << endl;
+  // effort_cmd_vr = this -> pid_vel_right.Update(vel_err_right, this -> step_time);
+
+  // cout << "\tErr_lef : " << vel_err_left << "\tpid_res: " << effort_cmd_vl << endl;
+  // cout << "\tErr_lef : " << vel_err_right << "\tpid_res: " << effort_cmd_vr << endl;
+
+  // this -> joint_left_wheel -> SetForce(0, effort_cmd_vl);
+  // this -> joint_right_wheel -> SetForce(0, effort_cmd_vr);
+  // this -> joint_controller -> SetVelocityTarget(this -> left_wheel_name, pid_res);
+  // this -> joint_controller -> SetVelocityTarget(this -> right_wheel_name, pid_res);
+}
+
+void autonomos_ideal_plugin::OnUpdate(const common::UpdateInfo &)
+{    
+  this -> prevUpdateTime = this -> current_time;
+  #if GAZEBO_VERSION_MAJOR >= 8
+    this -> current_time = this -> model -> GetWorld() -> SimTime();
+  #else
+    this -> current_time = this -> model -> GetWorld() -> GetSimTime();
+  #endif
+  this -> step_time = this -> current_time - this->prevUpdateTime;
+  // cout << "plugin OnUpdate" << endl;
+  if (!waiting_valid_traj)
+  {
+    common::Time diff_time = seg_fin_time - this -> world -> SimTime();
+
+    cout << "Segment: "    << trj_seg_msg.request.seq - 1 << "\tStep: " << this->step_time.Double() << endl;
+    cout << "\t Initial: " << seg_init_time.sec << "." << seg_init_time.nsec << endl;
+    cout << "\t   Final: " << seg_fin_time.sec << "." << seg_fin_time.nsec << endl;
+    cout << "\tDuration: " << trj_seg_msg.response.duration << endl;
+    cout << "\tSteering: " << trj_seg_msg.response.steering << endl;
+    cout << "\tVel_desi: " << this -> velocity_target << endl;
+    cout << "\tVel_real: " << this -> model -> RelativeLinearVel().X() << endl; 
+
+    if (diff_time <= 0.0 && this -> ros_service_client.call(trj_seg_msg))
+    {
+      if (trj_seg_msg.response.is_valid)
+      {
+        seg_init_time = this -> world -> SimTime();
+        seg_fin_time.Set(trj_seg_msg.response.duration);
+        seg_fin_time += seg_init_time;
+        
+        // this -> linear_vel.Set(trj_seg_msg.response.speed, 0, 0);
+        // this -> velocity_target = trj_seg_msg.response.speed;
+        this -> velocity_target = 0.5;
+        // this -> model -> SetLinearVel(this -> linear_vel);
+        this -> steering_pos_traget = trj_seg_msg.response.steering;
+
+        trj_seg_msg.request.seq++;
+
+      }
+      else
+      {
+        // waiting_valid_traj = true;
+        // trj_seg_msg.request.seq = 0;
+        this -> linear_vel.Set(0, 0, 0);
+        // this -> model -> SetLinearVel(this -> linear_vel);
+        this -> steering_pos_traget = 0;
+      }
+    }
+  }
+  else if(waiting_valid_traj && this -> ros_service_client.call(trj_seg_msg))
+  {
+
+    if(trj_seg_msg.response.is_valid)
+    {
+      // cout << "valid message" << endl;
+      seg_init_time =  this -> world -> SimTime();
+      seg_fin_time.Set(trj_seg_msg.response.duration);
+      seg_fin_time += seg_init_time;
+      
+      this -> linear_vel.Set(trj_seg_msg.response.speed, 0, 0);
+      // this -> model -> SetLinearVel(this -> linear_vel);
+      this -> steering_pos_traget = trj_seg_msg.response.steering;
+      
+      trj_seg_msg.request.seq++;
+      waiting_valid_traj = false;
+      
+      cout << "Segment: " << trj_seg_msg.request.seq - 1 << endl;
+      cout << "\t Initial: " << seg_init_time.sec << "." << seg_init_time.nsec << endl;
+      cout << "\t   Final: " << seg_fin_time.sec << "." << seg_fin_time.nsec << endl;
+      cout << "\tSteering: " << trj_seg_msg.response.steering << endl;
+      cout << "\tVelocity: " << trj_seg_msg.response.speed << endl;
+      cout << "\tDuration: " << trj_seg_msg.response.duration << endl;
+
+      // cout << "\tDiff: " << diff_time.sec << "." << diff_time.nsec << endl;
+    }
+    else
+    {
+      // cout << "NOT a valid message" << endl;
+    }
+  }
+  
+  SetPosition();
+  drive();
+}
+      
+/// \brief ROS helper function that processes messages
+void autonomos_ideal_plugin::QueueThread()
+{
+  static const double timeout = 0.01;
+  while (this->rosNode->ok())
+  {
+    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+  }
+}      
