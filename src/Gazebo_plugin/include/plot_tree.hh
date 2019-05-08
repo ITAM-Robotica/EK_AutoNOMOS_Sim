54ee79ddf4e5e749da5b2d71062e2b1252f574e1
Edgar Granados Osegueda <edgar.granados@itam.mx>
2019-05-08 18:44:32 -0500

    model plugin to plot lines

--- src/Gazebo_plugin/include/plot_tree.hh
+++ src/Gazebo_plugin/include/plot_tree.hh
@@ -2,24 +2,17 @@
 #define PLOT_TREE_HH_
 
 // std
-#include <memory>
 #include <mutex>
+#include <memory>
+#include <thread>
 
 // gazebo
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/common/Time.hh>
-#include <gazebo/common/Color.hh>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/Events.hh>
 #include <gazebo/physics/Model.hh>
-#include <gazebo/transport/Node.hh>
-// #include <gazebo/rendering/Visual.hh>
-
-// #include <gazebo/rendering/RenderTypes.hh>
-// #include <gazebo/rendering/DynamicLines.hh>
-// #include <rendering/rendering.hh>
-// #include <gazebo/rendering/Visual.hh>
-// #include <gazebo/rendering/Scene.hh>
+#include <gazebo_plugins/PubQueue.h>
 
 // ignition
 #include <ignition/math.hh>
@@ -33,11 +26,16 @@
 
 // gazebo_plugin
 #include "gazebo_plugin/gz_visual_line.h"
+#include "gazebo_plugin/Line_Segment.h"
+
 
 // namespace gazebo
 // {
 using namespace gazebo;
 
+// class plot_tree_plugin : public WorldPlugin
+// class plot_tree_plugin : public SystemPlugin
+// class GAZEBO_VISIBLE plot_tree_plugin : public VisualPlugin
 class plot_tree_plugin : public ModelPlugin
 {
     /// \brief Constructor.
@@ -47,19 +45,37 @@ class plot_tree_plugin : public ModelPlugin
   public: ~plot_tree_plugin();
 
   // Documentation inherited
-  public: virtual void Load(physics::ModelPtr _model,
-      sdf::ElementPtr _sdf);
+  public: 
+    // virtual void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf );
+    // virtual void Load(int /*_argc*/, char ** /*_argv*/);
+    // virtual void Load(rendering::VisualPtr _visual, sdf::ElementPtr _sdf);
+    virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
+  void OnUpdate(const common::UpdateInfo & /*_info*/);
   /// \brief Update the plugin once every iteration of simulation.
   // private: void Update();
 
   /// \brief Callback to receive info.
   // private: void OnInfo(ConstPosesStampedPtr &_msg);
 
-  private: bool get_next_line_segment(
-                gazebo_plugin::gz_visual_line::Request &req,
-                gazebo_plugin::gz_visual_line::Response &res);
+  private: //bool get_next_line_segment(
+           //     gazebo_plugin::gz_visual_line::Request &req,
+           //     gazebo_plugin::gz_visual_line::Response &res);
 
+  void get_next_line_segment(const gazebo_plugin::Line_SegmentConstPtr &msg);
+
+  void QueueThread();
+
+  ros::Subscriber rosSub;
+  
+  ros::CallbackQueue rosQueue;
+
+  std::thread rosQueueThread;
+
+  ros::Subscriber sub_start;
+
+  // std::vector<gazebo_plugin::Line_SegmentPtr> lines_queue;
+  std::queue<gazebo_plugin::Line_Segment> lines_queue;
   /// \internal
   /// \brief Private data pointer
   // private: std::unique_ptr<plot_tree_plugin_private> dataPtr;
@@ -75,6 +91,19 @@ class plot_tree_plugin : public ModelPlugin
   private: physics::ModelPtr model;
 
   private: int id;
+
+  event::ConnectionPtr updateConnection;
+
+  public: 
+    // std::unique_ptr<rendering::DynamicLines> dynamicRenderable;
+    rendering::DynamicLines *dynamicRenderable;
+
+  rendering::VisualPtr world_visual_ptr;
+
+  public: bool loaded = false;
+  // private: MarkerVisualPrivate *dPtr;
+
+
   /// \brief For example a line to visualize the force
   // private: rendering::DynamicLines *line;
 
--- src/Gazebo_plugin/src/plot_tree.cc
+++ src/Gazebo_plugin/src/plot_tree.cc
@@ -2,10 +2,13 @@
 
 using std::endl;
 using std::cout;
+using std::string;
 using namespace gazebo;
 
 GZ_REGISTER_MODEL_PLUGIN(plot_tree_plugin)
-
+// GZ_REGISTER_WORLD_PLUGIN(plot_tree_plugin)
+// GZ_REGISTER_SYSTEM_PLUGIN(plot_tree_plugin)
+// GZ_REGISTER_VISUAL_PLUGIN(plot_tree_plugin)
 /////////////////////////////////////////////////
 plot_tree_plugin::plot_tree_plugin()
 {
@@ -24,26 +27,16 @@ plot_tree_plugin::~plot_tree_plugin()
 }
 
 /////////////////////////////////////////////////
+// void plot_tree_plugin::Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
+// void plot_tree_plugin::Load(int /*_argc*/, char ** /*_argv*/)
+// void plot_tree_plugin::Load(rendering::VisualPtr _visual, sdf::ElementPtr _sdf)
 void plot_tree_plugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 {
   gzdbg  << "plot_tree_plugin: loading..." << endl;
   GZ_ASSERT(_model, "LinkPlot3DPlugin _model pointer is NULL");
   GZ_ASSERT(_sdf, "LinkPlot3DPlugin _sdf pointer is NULL");
   
-  // std::vector<std::string> serviceList;
-  // this -> ign_node.ServiceList(serviceList);
-
-  // if (std::find(serviceList.begin(), serviceList.end(), "/marker")
-  //     == serviceList.end())
-  // {
-  //   gzerr << "Error: /marker service not present on network.\n";
-  //   return;
-  // }
-
-  // this -> ign_node.Advertise<ignition::msgs::Marker>("/marker");
-
   this -> model = _model;
-  // this -> visual = _visual;
 
   if (!ros::isInitialized())
   {
@@ -54,268 +47,109 @@ void plot_tree_plugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   gzdbg << "plot_tree_plugin: ROS initialized..." << endl;
 
   // // Create our ROS node. This acts in a similar manner to the Gazebo node
-  ros::NodeHandle nh("plot_tree_plugin_nh"); 
-  this->rosNode.reset(&nh);
+  this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
   
-  // trj_seg_msg.request.seq = 0;
-  this -> ros_service_server = nh.advertiseService("/gz_visual/line_segment", 
-    &plot_tree_plugin::get_next_line_segment, this);
-    // boost::bind(&plot_tree_plugin::get_next_line_segment, this, _1, _2));
+  
+  string topic_name = "/" + this -> model -> GetName() + "/gz_visual/line_segment";
 
-  // ros::ServiceClientOptions sco = ros::ServiceClientOptions::init(srv_name, false, header);
-  // this -> ros_service_client = nh.serviceClient<gazebo_plugin::trajectory_segment>("/navigation/get_next_control");
+  cout << "topic_name: " << topic_name << endl;
+  ros::SubscribeOptions so =
+    ros::SubscribeOptions::create<gazebo_plugin::Line_Segment>(
+      topic_name,
+      100000,
+      boost::bind(&plot_tree_plugin::get_next_line_segment, this, _1),
+      ros::VoidPtr(), NULL
+      // , &this->rosQueue
+      );
 
-  // Spin up the queue helper thread.
-  // this->rosQueueThread = std::thread(std::bind(&autonomos_ideal_plugin::QueueThread, this));
+  this->rosSub = this->rosNode->subscribe(so);
+  
+  ros::AsyncSpinner spinner(2); // Use 4 threads
+  spinner.start();
+  // this->rosQueueThread =
+    // std::thread(std::bind(&plot_tree_plugin::QueueThread, this));
 
-  // this -> ideal_move_thread = std::thread(std::bind(&autonomos_ideal_plugin::move_robot, this));
-  // Listen to the update event. This event is broadcast every
-  // simulation iteration.
-  // this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-    // boost::bind(&autonomos_ideal_plugin::OnUpdate, this, _1));
-  // this -> model -> SetJointPosition("steer_joint", 0);
-
-
-  // Message
-  // ignition::msgs::Marker markerMsg;
-  // markerMsg.set_ns("tree_" + this -> model -> GetName());
-  // markerMsg.set_id(0);
-  // markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  // markerMsg.set_type(ignition::msgs::Marker::LINE_LIST);
-
-  // std::string mat;
-  // mat = "Gazebo/Black";
-  // ignition::msgs::Material *matMsg = markerMsg.mutable_material();
-  // matMsg->mutable_script()->set_name(mat);
-
-  // // this -> msg = markerMsg;
-  // ignition::msgs::Set(markerMsg.add_point(),
-  //   ignition::math::Vector3d(2,2,0));
-  // ignition::msgs::Set(markerMsg.add_point(),
-  //   ignition::math::Vector3d(1,1,0));
-  // this -> ign_node.Request("/marker", markerMsg);
-
-  // if (!this -> ign_node.Request("/marker", msg))
-  //   gzerr << "Error adding a marker\n";
-
-  // gzdbg << "plot_tree_plugin: plugin loaded" << endl;
+  this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+    boost::bind(&plot_tree_plugin::OnUpdate, this, _1));
 
 }
 
-bool plot_tree_plugin::get_next_line_segment(gazebo_plugin::gz_visual_line::Request &req,
-                 gazebo_plugin::gz_visual_line::Response &res)
+void plot_tree_plugin::get_next_line_segment(const gazebo_plugin::Line_SegmentConstPtr &msg)
 {
-  // this->line = this -> visual -> CreateDynamicLine(rendering::RENDERING_LINE_STRIP);
 
-  gzdbg << req << endl;
+  gzdbg << __PRETTY_FUNCTION__ << endl;
+  gazebo_plugin::Line_Segment aux;
+  aux.header    = msg -> header;
+  aux.line_list = msg -> line_list;
+  aux.points    = msg -> points;
+  aux.color     = msg -> color;
+  aux.action    = msg -> action;
+  this -> lines_queue.push(aux);
 
-  // ignition::msgs::Marker markerMsg;
-  // ignition::msgs::Material *matMsg = markerMsg.mutable_material();
-
-  // matMsg->mutable_script()->set_name("Gazebo/Blue");
-  // markerMsg.set_id(2);
-  // markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  // markerMsg.set_type(ignition::msgs::Marker::LINE_STRIP);
-  // // markerMsg.clear_point();
-
-  // ignition::msgs::Set(markerMsg.add_point(),
-  //   ignition::math::Vector3d(req.start.x, req.start.y, req.start.z));
-  // ignition::msgs::Set(markerMsg.add_point(),
-  //   ignition::math::Vector3d(req.end.x, req.end.y, req.end.z));
-
-  // // this->dataPtr->node.Request("/marker", plot.msg);
-  // this -> ign_node.Request("/marker", markerMsg);
-
-  // res.success = 1;
-  
-  // if (!this -> ign_node.Request("/marker", msg))
-  // {
-  //   gzerr << "Error adding a marker\n";
-  // } 
-  // else 
-  // {
-  //   gzdbg << "request successful" << endl;
-  // }
-
-  ignition::transport::Node node;
-
-  // Create the marker message
-  ignition::msgs::Marker markerMsg;
-  markerMsg.set_ns("default");
-  markerMsg.set_id(0);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::SPHERE);
-
-  ignition::msgs::Material *matMsg = markerMsg.mutable_material();
-  matMsg->mutable_script()->set_name("Gazebo/BlueLaser");
-
-  // The rest of this function adds different shapes and/or modifies shapes.
-  // Read the terminal statements to figure out what each node.Request
-  // call accomplishes.
-
-  std::cout << "Spawning a sphere at the origin\n";
-  gazebo::common::Time::Sleep(4);
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Moving the sphere to x=0, y=0, z=1\n";
-  gazebo::common::Time::Sleep(4);
-  ignition::msgs::Set(markerMsg.mutable_pose(),
-                      ignition::math::Pose3d(0, 0, 1, 0, 0, 0));
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Shrinking the sphere\n";
-  gazebo::common::Time::Sleep(4);
-  ignition::msgs::Set(markerMsg.mutable_scale(),
-                    ignition::math::Vector3d(0.2, 0.2, 0.2));
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Changing the sphere to red\n";
-  gazebo::common::Time::Sleep(4);
-  matMsg->mutable_script()->set_name("Gazebo/Red");
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding a green box\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(1);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::BOX);
-  matMsg->mutable_script()->set_name("Gazebo/Green");
-  ignition::msgs::Set(markerMsg.mutable_scale(),
-                    ignition::math::Vector3d(1.0, 1.0, 1.0));
-  ignition::msgs::Set(markerMsg.mutable_pose(),
-                    ignition::math::Pose3d(2, 0, .5, 0, 0, 0));
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Change the green box to a cylinder\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_type(ignition::msgs::Marker::CYLINDER);
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding a line between the sphere and cylinder\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(2);
-  ignition::msgs::Set(markerMsg.mutable_pose(),
-                    ignition::math::Pose3d(0, 0, 0, 0, 0, 0));
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::LINE_LIST);
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(0, 0, 1.1));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(2, 0, 0.5));
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding a square around the origin\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(3);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::LINE_STRIP);
-  ignition::msgs::Set(markerMsg.mutable_point(0),
-      ignition::math::Vector3d(0.5, 0.5, 0.05));
-  ignition::msgs::Set(markerMsg.mutable_point(1),
-      ignition::math::Vector3d(0.5, -0.5, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(-0.5, -0.5, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(-0.5, 0.5, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(0.5, 0.5, 0.05));
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding 100 points inside the square\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(4);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::POINTS);
-  markerMsg.clear_point();
-  for (int i = 0; i < 100; ++i)
-  {
-    ignition::msgs::Set(markerMsg.add_point(),
-        ignition::math::Vector3d(
-          ignition::math::Rand::DblUniform(-0.49, 0.49),
-          ignition::math::Rand::DblUniform(-0.49, 0.49),
-          0.05));
-  }
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding a semi-circular triangle fan\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(6);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::TRIANGLE_FAN);
-  markerMsg.clear_point();
-  ignition::msgs::Set(markerMsg.mutable_pose(),
-                    ignition::math::Pose3d(0, 1.5, 0, 0, 0, 0));
-  ignition::msgs::Set(markerMsg.add_point(),
-        ignition::math::Vector3d(0, 0, 0.05));
-  double radius = 2;
-  for (double t = 0; t <= M_PI; t+= 0.01)
-  {
-    ignition::msgs::Set(markerMsg.add_point(),
-        ignition::math::Vector3d(radius * cos(t), radius * sin(t), 0.05));
-  }
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding two triangles using a triangle list\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(7);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::TRIANGLE_LIST);
-  markerMsg.clear_point();
-  ignition::msgs::Set(markerMsg.mutable_pose(),
-                    ignition::math::Pose3d(0, -1.5, 0, 0, 0, 0));
-  ignition::msgs::Set(markerMsg.add_point(),
-        ignition::math::Vector3d(0, 0, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(1, 0, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(1, 1, 0.05));
-
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(1, 1, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(2, 1, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(2, 2, 0.05));
-
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Adding a rectangular triangle strip\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_id(8);
-  markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
-  markerMsg.set_type(ignition::msgs::Marker::TRIANGLE_STRIP);
-  markerMsg.clear_point();
-  ignition::msgs::Set(markerMsg.mutable_pose(),
-                    ignition::math::Pose3d(-2, -2, 0, 0, 0, 0));
-  ignition::msgs::Set(markerMsg.add_point(),
-        ignition::math::Vector3d(0, 0, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(1, 0, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(0, 1, 0.05));
-
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(1, 1, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(0, 2, 0.05));
-  ignition::msgs::Set(markerMsg.add_point(),
-      ignition::math::Vector3d(1, 2, 0.05));
-
-  node.Request("/marker", markerMsg);
-
-  std::cout << "Delete all the markers\n";
-  gazebo::common::Time::Sleep(4);
-  markerMsg.set_action(ignition::msgs::Marker::DELETE_ALL);
-  node.Request("/marker", markerMsg);
-
-  return true;
 }
 
 /////////////////////////////////////////////////
-// void plot_tree_plugin::Update()
-// {
+void plot_tree_plugin::OnUpdate(const common::UpdateInfo & /*_info*/)
+{
 
-// }
+  std::hash<string> ptr_hash;
+  ignition::transport::Node node;
+  ignition::msgs::Marker markerMsg;
+  markerMsg.set_ns(this -> model -> GetName());
+
+  gazebo_plugin::Line_Segment msg;// = this -> lines_queue.front();
+  ignition::msgs::Material *matMsg = markerMsg.mutable_material();
+  // while(! this -> lines_queue.empty())
+  if(! this -> lines_queue.empty())
+  {
+
+    msg = this -> lines_queue.front();
+    this -> lines_queue.pop();
+    markerMsg.set_id(ptr_hash(msg.header.frame_id));
+    // TODO: separate ids and use msg add_modify
+    markerMsg.set_action(ignition::msgs::Marker::ADD_MODIFY);
+    
+    if (msg.line_list)
+    {
+      markerMsg.set_type(ignition::msgs::Marker::LINE_LIST);
+    }
+    else
+    {
+      markerMsg.set_type(ignition::msgs::Marker::LINE_STRIP);
+    }
+
+    matMsg->mutable_script()->set_name(msg.color);
+
+    for (auto&  pt : msg.points) 
+    {
+      // gzdbg << "msg: " << msg.header.frame_id << " "
+      //   << pt.x << " " << pt.y << " " << pt.z << " "
+      //   << endl;
+      
+      ignition::msgs::Set(markerMsg.add_point(),
+          ignition::math::Vector3d(pt.x, pt.y, pt.z));
+    }
+
+    // gzdbg << "msg: " << msg.header.frame_id << " "
+    //   << msg.start.x << " " << msg.start.y << " " << msg.start.z << " "
+    //   << msg.end.x   << " " << msg.end.y   << " " << msg.end.z
+    //   << endl;
+
+    node.Request("/marker", markerMsg);
+  }
+  
+}
+
+void plot_tree_plugin::QueueThread()
+{
+  static const double timeout = 0.01;
+  // 
+  while (this->rosNode->ok())
+  {
+    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+  }
+}   
 
 /////////////////////////////////////////////////
 // void plot_tree_plugin::OnInfo(ConstPosesStampedPtr &_msg)
